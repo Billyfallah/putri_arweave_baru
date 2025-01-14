87d88a60fd6abfdacd2f120988d80800369a3b8a
Billyfallah <100917152+Billyfallah@users.noreply.github.com>
2025-01-14 08:37:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+waktu masih panjang bang jadi jangan nyerah tetap semanagat
+lama banget nih dapetnya 
+siapin dulu roko jarcok dan juga kponya pasti bang
+tunggu saya sampe lv 15 bang jangan di tinggallah ko sepi sih ga kaya malem rame banget
+apalagi dah udah banyak yang lupa
+nyanyi aja ga sih biar ga pusing
+mencetin hp hpnya belek barry 
+temen nongkrong di kafe gua di ??
+ah gabut bener bang
+waktu masih panjang bang jadi jangan nyerah tetap 
