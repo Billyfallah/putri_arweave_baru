292452fbe13ddf1f45cc2c74ed8aa333fa49dc98
Billyfallah <100917152+Billyfallah@users.noreply.github.com>
2025-01-15 10:26:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("ctaButton").addEventListener("click", function() {
+  window.location.href = "https://www.arlink.io";  // Redirect to ARlink's website or any other page
+});
