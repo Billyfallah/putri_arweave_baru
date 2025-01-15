4aebbd15d79de4020881924d359b9bdfbbd299c4
Billyfallah <100917152+Billyfallah@users.noreply.github.com>
2025-01-15 07:54:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-waktu masih panjang bang jadi jangan nyerah tetap semanagat
-lama banget nih dapetnya 
-siapin dulu roko jarcok dan juga kponya pasti bang
-tunggu saya sampe lv 15 bang jangan di tinggallah ko sepi sih ga kaya malem rame banget
-apalagi dah udah banyak yang lupa
-nyanyi aja ga sih biar ga pusing
-mencetin hp hpnya belek barry 
-temen nongkrong di kafe gua di ??
-ah gabut bener bang
-waktu masih panjang bang jadi jangan nyerah tetap 
+function showMessage() {
+    alert("Welcome to the Professional Arweave Website!");
+}
